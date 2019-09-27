13d77b84f2276d9f59925e8ec2e166634103c800
Mrrwmix <seidoc@gmail.com>
2019-09-26 20:24:50 -0700

    Registration page form complete, need to work on functionality now

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,13 +1,72 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { Fragment } from "react";
 
 function Register() {
-    return (
-        <div>
-            <p>Register page</p>
-            <Link to="/">back to home</Link>
+  return (
+    <Fragment>
+      <div className='row'>
+        <h1 className='col-md-12 text-center'>Register Below</h1>
+      </div>
+      <div className='form-group row'>
+        <label for='Username' class='col-md-3 col-form-label'>
+          Username
+        </label>
+        <div className='col-md-9'>
+          <input
+            type='text'
+            name='Username'
+            id='uname'
+            className='form-control'
+            placeholder='Enter your username for the Code Timebank here'
+          />
         </div>
-    )
+      </div>
+      <div className='form-group row'>
+        <label for='Password' class='col-md-3 col-form-label'>
+          Password
+        </label>
+        <div className='col-md-9'>
+          <input
+            type='password'
+            name='Password'
+            id='pword'
+            className='form-control'
+            placeholder='Super secret password'
+          />
+        </div>
+      </div>
+      <div className='form-group row'>
+        <label for='Github' class='col-md-3 col-form-label'>
+          Github Username
+        </label>
+        <div className='col-md-9'>
+          <input
+            type='text'
+            name='Github'
+            id='ghubname'
+            className='form-control'
+            placeholder='Enter your Github username here'
+          />
+        </div>
+      </div>
+      <div className='form-group row'>
+        <label for='Discord' class='col-md-3 col-form-label'>
+          Discord Username
+        </label>
+        <div className='col-md-9'>
+          <input
+            type='text'
+            name='Discord'
+            id='discord'
+            className='form-control'
+            placeholder='Username#1234'
+          />
+        </div>
+      </div>
+      <button className='btn btn-block btn-secondary' onClick={""}>
+        Submit
+      </button>
+    </Fragment>
+  );
 }
 
-export default Register
+export default Register;
