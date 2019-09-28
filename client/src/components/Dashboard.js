ea993646f9855c78d51158ce31a18c446ec88fe4
armjim14 <ajimenez1497@gmail.com>
2019-09-28 15:54:04 -0700

    got structure set up i believe

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import QuestionContext from "../Context/question/questionContext";
 
 const Dashboard = () => {
+
+  const QuestionContext = useContext(QuestionContext);
+  const { sendQuestion } = QuestionContext;
+
+  const [questionAsked, setUser] = useState({
+    question: "",
+    language: "",
+    comfort: ""
+  });
+
+  const { question, language, comfort } = questionAsked;
+
+  const onChange = e => setUser({ ...questionAsked, [e.target.name]: e.target.value });
 
   const submit = e => {
     e.preventDefault();
@@ -10,8 +24,12 @@ const Dashboard = () => {
 
   return (
     <div>
+      <h1>What question do you have in mind</h1>
       <form onSubmit={submit}>
-
+        <input type="text" required name="question" />
+        <input type="text" required name="language" />
+        <input type="text" required name="comfort" />
+        <input type="submit" />
       </form>
     </div>
   );
