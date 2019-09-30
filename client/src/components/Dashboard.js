97de843ca8cd1e7f50f66c04ba6fd4472cb23a79
armjim14 <ajimenez1497@gmail.com>
2019-09-30 15:22:25 -0700

    pie is showing

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
+import SimplePieChart from "./SimplePieChart";
 
 const Dashboard = () => {
 
   return (
     <div>
       <p>Profile page</p>
+      <SimplePieChart/>
     </div>
   );
 
