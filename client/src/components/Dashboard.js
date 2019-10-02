6fce3784a247f31e44928619c882b46f37f22cb1
armjim14 <ajimenez1497@gmail.com>
2019-10-02 16:02:41 -0700

    info showing in dashbaord

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,45 @@
-import React, { Fragment } from "react";
+import React, { Fragment,useContext, useEffect, useState } from "react";
 import SimplePieChart from "./SimplePieChart";
+import AuthContext from "../Context/auth/authContext";
 
 const Dashboard = () => {
+
+  const [info, updateInfo] = useState({
+    name: "",
+    hours: 0
+  })
+
+  const authContext = useContext(AuthContext);
+  const { getUsernames } = authContext;
+
+  const getHours = () => {
+
+    console.log(info.hours)
+
+    if (info.hours){
+      console.log("Yes")
+      return <span>{info.hours}</span>
+    } else {
+      console.log("No")
+      return <span>0</span>
+    }
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+
+      let { github, credits } = await getUsernames();
+
+      updateInfo({
+        name: github,
+        hours: credits
+      });
+
+    }
+    fetchData();
+    //eslint-disable-next-line
+  }, []);
+
   return (
     <Fragment>
       <div className='row'>
@@ -9,6 +47,7 @@ const Dashboard = () => {
           <h1 className='text-center text-black'>Dashboard</h1>
         </div>
       </div>
+
       <div className='row'>
         <div className='col-md-6'>
           <a href='#!' style={{ textDecoration: "none" }}>
@@ -25,10 +64,22 @@ const Dashboard = () => {
           </a>
         </div>
       </div>
-      <div>
-        <p>Profile page</p>
-        <SimplePieChart />
+
+      <div className="row mt-4">
+        <div className="col-md-12">
+          <h2 className="text-center">Hello, {info.name}</h2>
+        </div>
       </div>
+
+      <div className="row">
+        <div className="col-md-6">
+          <SimplePieChart />
+        </div>
+        <div className="col-md-6 pt-5">
+          <p className="text-center pt-5">Credits {getHours()}</p>
+        </div>
+      </div>
+
     </Fragment>
   );
 };
