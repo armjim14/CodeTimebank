218ec8639121b7301842ca52389380d7fa9927ba
Mrrwmix <seidoc@gmail.com>
2019-10-01 20:16:05 -0700

    Frontend part of edit profile done

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
 import SimplePieChart from "./SimplePieChart";
 
 const Dashboard = () => {
-
   return (
-    <div>
-      <p>Profile page</p>
-      <SimplePieChart/>
-    </div>
+    <Fragment>
+      <div className='row'>
+        <div className='col-md-12'>
+          <h1 className='text-center text-black'>Dashboard</h1>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-md-6'>
+          <a href='#!' style={{ textDecoration: "none" }}>
+            <button className='btn btn-block btn-beige rounded-pill'>
+              View Profile
+            </button>
+          </a>
+        </div>
+        <div className='col-md-6'>
+          <a href='/editprofile' style={{ textDecoration: "none" }}>
+            <button className='btn btn-block btn-greyish rounded-pill'>
+              Edit Profile
+            </button>
+          </a>
+        </div>
+      </div>
+      <div>
+        <p>Profile page</p>
+        <SimplePieChart />
+      </div>
+    </Fragment>
   );
-
 };
 
 export default Dashboard;
--- client/src/components/EditProfile.js
+++ client/src/components/EditProfile.js
@@ -3,16 +3,46 @@ import React, { Fragment, useContext, useState, useEffect } from "react";
 const EditProfile = () => {
   const [info, setInfo] = useState({
     skype: "",
-    github: ""
+    github: "",
+    discord: ""
   });
+  const { skype, github, discord } = info;
+
+  const onChange = e => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
+
   return (
     <Fragment>
       <div className='row'>
-        <h1 className='text-center text-black'>Edit Your Information</h1>
+        <div className='col-md-12'>
+          <h1 className='text-center text-black'>Edit Your Information</h1>
+        </div>
+        <div className='col-md-12'>
+          <h6 className='text-center text-dbrown small'>
+            Verify, change, or add your usernames for the following
+          </h6>
+        </div>
       </div>
       <div className='form-group row'>
-        <label htmlFor='skype' className='col-md-2 col-form-label'></label>
-        <div className='col-md-10'>
+        <label htmlFor='github' className='col-md-1 col-form-label'>
+          Github
+        </label>
+        <div className='col-md-11'>
+          <input
+            type='text'
+            name='github'
+            className='form-control'
+            value={github}
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <div className='form-group row'>
+        <label htmlFor='skype' className='col-md-1 col-form-label'>
+          Skype
+        </label>
+        <div className='col-md-11'>
           <input
             type='text'
             name='skype'
@@ -22,6 +52,24 @@ const EditProfile = () => {
           />
         </div>
       </div>
+      <div className='form-group row'>
+        <label htmlFor='discord' className='col-md-1 col-form-label'>
+          Discord
+        </label>
+        <div className='col-md-11'>
+          <input
+            type='text'
+            name='discord'
+            className='form-control'
+            value={discord}
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <button className='btn btn-block btn-greyish'>Submit</button>
+      <a href='/dashboard' style={{ textDecoration: "none" }}>
+        <button className='btn btn-block btn-dbrown'>Cancel</button>
+      </a>
     </Fragment>
   );
 };
