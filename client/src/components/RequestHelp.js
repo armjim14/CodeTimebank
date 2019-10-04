c051a996d4d727d1771951f43b82fadb2fddfdec
Mrrwmix <seidoc@gmail.com>
2019-10-03 18:25:11 -0700

    Changed to a select input

--- client/src/components/RequestHelp.js
+++ client/src/components/RequestHelp.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import alertContext from "../Context/alert/alertContext";
 import questionContext from "../Context/question/questionContext";
+import languages from "./data/languages";
 
 function RequestHelp() {
   const QuestionContext = useContext(questionContext);
@@ -13,6 +14,7 @@ function RequestHelp() {
     language: "",
     topic: ""
   });
+  console.log(languages);
 
   const { question, language, topic } = questionAsked;
 
@@ -22,7 +24,7 @@ function RequestHelp() {
   const submit = e => {
     e.preventDefault();
 
-    console.log("I was clicked")
+    console.log("I was clicked");
 
     if (question === "" || language === "" || topic === "") {
       setAlert("Fill in all fields", "danger");
@@ -35,38 +37,50 @@ function RequestHelp() {
     <div>
       <h1>What question are you wanting to ask</h1>
       <form onSubmit={submit}>
-        Question:{" "}
-        <input
-          onChange={onChange}
-          value={question}
-          type='text'
-          required
-          name='question'
-        />
-        <br />
-        <br />
-        Langugage:{" "}
-        <input
-          onChange={onChange}
-          value={language}
-          type='text'
-          required
-          name='language'
-        />
-        <br />
-        <br />
-        topic:{" "}
-        <input
-          onChange={onChange}
-          value={topic}
-          type='text'
-          required
-          name='topic'
-        />
-        <br />
-        <br />
+        <div className='row'>
+          <div className='form-group col-md-6'>
+            <label htmlFor='topic'>Topic: </label>
+            <input
+              onChange={onChange}
+              value={topic}
+              type='text'
+              required
+              name='topic'
+              className='form-control'
+              placeholder="What's the general issue you're having?"
+            />
+          </div>
+          <div className='form-group col-md-6'>
+            <label htmlFor='language'>Language: </label>
+            <select
+              onChange={onChange}
+              required
+              name='language'
+              className='form-control'
+            >
+              <option value=''>Select a language</option>
+              {languages.map(language => (
+                <option value={language.name} key={language.name}>
+                  {language.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='question'>Question:</label>
+          <textarea
+            onChange={onChange}
+            value={question}
+            required
+            name='question'
+            className='form-control'
+            placeholder='Describe your issue in detail.'
+            rows='5'
+          />
+        </div>
         <input type='submit' />
-        <br />
       </form>
     </div>
   );
