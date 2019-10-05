2ece356775b364ce7ac5889b151d7cbe3cc409db
George <49299319+GeorgeCampa@users.noreply.github.com>
2019-10-05 13:25:57 -0700

    Add files via upload

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE timebank;
+USE timebank;
+
+CREATE TABLE users
+	(
+id int NOT NULL AUTO_INCREMENT, 
+username VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+github VARCHAR(255) NOT NULL,
+discord VARCHAR(255) NOT NULL,
+skype VARCHAR(255) NOT NULL,
+credits VARCHAR(255) NOT NULL,
+createdAt timesteamp default current_timestamp,
+updatedAt timesteamp default current_timestamp,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE questions
+
+(
+id int NOT NULL AUTO_INCREMENT,
+language VARCHAR(255) NOT NULL ,
+topic VARCHAR (255) NOT NULL ,
+createdAt VARCHAR(255) NOT NULL ,
+updatedAt VARCHAR(255) NOT NULL ,
+UserId FOREIGN KEY
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (username, passsword) VALUES ('username');
+INSERT INTO users (complete) VALUES ('password');
+INSERT INTO users (username, passsword) VALUES ('github');
+INSERT INTO users (username, passsword) VALUES ('discord');
+INSERT INTO users (username, passsword) VALUES ('skype');
+
+INSERT INTO questions (question, language, topic) VALUES ('Please enter a question' );
+INSERT INTO questions (question, language, topic) VALUES ('Please provide a language');
+INSERT INTO questions (question, language, topic) VALUES ('Please provide your comfort level');
