21507d89df6a9de94974e237964f7a13330c9738
Ruben Valdez <ruben.valdez7@gmail.com>
2019-10-09 21:10:05 -0700

    adding mock strings into type library

--- client/src/components/Splash.js
+++ client/src/components/Splash.js
@@ -1,10 +1,33 @@
 import React from 'react'
-import WordCloud from './WordCloud'
+import { render } from 'react-dom';
+import Typed from 'react-typed';
+
+
+// import WordCloud from './WordCloud'
 
 const Splash = () => {
     return (
         <div>
-            <WordCloud />
+            <img className="splashImage" src="">
+            {/* <WordCloud /> */}
+            <Typed
+                    strings={['Here you can find anything']}
+                    typeSpeed={40}
+                />
+                <br/>
+ 
+                <Typed
+                strings={[
+                    'Search for products',
+                    'Search for categories',
+                    'Search for brands']}
+                    typeSpeed={40}
+                    backSpeed={50}
+                    attr="placeholder"
+                    loop >
+                    <input type="text"/>
+                </Typed>
+                </img>
         </div>
 
 )
