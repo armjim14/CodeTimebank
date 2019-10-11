dedf9305b3c4e20899624021e587c2e1c9d19f11
Ruben Valdez <ruben.valdez7@gmail.com>
2019-10-10 20:52:28 -0700

    overlay working

--- client/src/components/Splash.js
+++ client/src/components/Splash.js
@@ -3,13 +3,13 @@ import React from 'react'
 import Typed from 'react-typed';
 import './Splash.css'
 
-import WordCloud from './WordCloud'
+// import WordCloud from './WordCloud'
 
 const Splash = () => {
     return (
         <div >
-            <img src="./images/Splash.jpg" />
-            <h1 className="typed">
+            <img className="splashimage" src="./images/Splash.jpg" />
+            {/* <h1 className="typed"> */}
             <Typed 
             className="typed"
             strings= {['Welcome to code time bank',
@@ -17,12 +17,13 @@ const Splash = () => {
                         'Help others',
                         'Earn time for your bank',
                         'Ready to Code?']}
-                        typeSpeed={30}
-                        backSpeed={40}
-                        backDelay={20}
+                        typeSpeed={80}
+                        backSpeed={30}
+                        backDelay={300}
                         loop>
 
-                        </Typed></h1>
+                        </Typed>
+                        {/* </h1> */}
                 
         </div>
 
