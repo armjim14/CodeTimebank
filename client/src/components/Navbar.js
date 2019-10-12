20a6349832ec9572ccc63d33d3355a1cada3f621
armjim14 <ajimenez1497@gmail.com>
2019-10-12 14:01:18 -0700

    logo color is next

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -103,7 +103,7 @@ function Navbar(props) {
           className='collapse navbar-collapse'
           id='navbarSupportedContent'
         >
-          <ul className='navbar-nav m-auto'>
+          <ul className='navbar-nav ml-auto'>
             <li className='nav-item'>
               <Link
                 style={style.forTextOdd}
