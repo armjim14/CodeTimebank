eeb8b88e625c571d6ba7b69e5c70f3a24da95217
armjim14 <ajimenez1497@gmail.com>
2019-10-14 14:59:52 -0700

    working on iphone 5

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -84,7 +84,7 @@ function Navbar(props) {
       className='container-fluid'
     >
       <nav style={{zIndex: "10"}} className='row navbar navbar-expand-lg navbar-light bg-gradient-mariner py-2'>
-        <a style={{fontSize: "1.2rem", color: "white"}} className="navbar-brand changeFont" href="/">
+        <a style={{color: "white"}} className="navbar-brand changeFont NavFont" href="/">
           <img className="mr-3 d-inline-block align-center" style={{width: "30px", paddingBottom: "10px"}} src="/images/realLogo.png" alt="logo" />
            Code Timebank
         </a>
