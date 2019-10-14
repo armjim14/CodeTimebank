77fd510f86845b0ec44fe2399f80334cc710d31a
Mrrwmix <seidoc@gmail.com>
2019-10-14 13:21:46 -0700

    Centered and minimized about and request forms

--- client/src/components/RequestHelp.js
+++ client/src/components/RequestHelp.js
@@ -20,7 +20,7 @@ function RequestHelp(props) {
 
   const onChange = e => {
     setQuestion({ ...questionAsked, [e.target.name]: e.target.value });
-  }
+  };
 
   const submit = async e => {
     e.preventDefault();
@@ -41,28 +41,33 @@ function RequestHelp(props) {
 
   return (
     <div>
-      <h1 style={{marginTop: "90px"}} className='text-center'>Request Help</h1>
+      <h1 style={{ marginTop: "90px" }} className='text-center'>
+        Request Help
+      </h1>
       <form onSubmit={submit}>
         <div className='row'>
-          <div className='form-group col-md-6'>
-            <label htmlFor='topic'>Topic: </label>
+          <div className='col-md-4'></div>
+          <div className='form-group col-md-4 text-center'>
+            <label htmlFor='topic font-weight-bold'>Topic: </label>
             <input
               onChange={onChange}
               value={topic}
               type='text'
               required
               name='topic'
-              className='form-control'
+              className='form-control text-center'
               placeholder="What's the general issue you're having?"
             />
           </div>
-          <div className='form-group col-md-6'>
-            <label htmlFor='language'>Language: </label>
+          <div className='col-md-4'></div>
+          <div className='col-md-4'></div>
+          <div className='form-group col-md-4 text-center'>
+            <label htmlFor='language font-weight-bold'>Language: </label>
             <select
               onChange={onChange}
               required
               name='language'
-              className='form-control'
+              className='form-control text-center'
             >
               <option value=''>Select a language</option>
               {languages.map(language => (
@@ -72,40 +77,46 @@ function RequestHelp(props) {
               ))}
             </select>
           </div>
+          <div className='col-md-4'></div>
+        </div>
+        <div className='row'>
+          <div className='col-md-3'></div>
+          <div className='form-group col-md-6 text-center'>
+            <label htmlFor='question font-weight-bold'>Question:</label>
+            <textarea
+              onChange={onChange}
+              value={question}
+              required
+              name='question'
+              className='form-control'
+              placeholder='Describe your problem in detail.'
+              rows='5'
+            />
+          </div>
+          <div className='col-md-3'></div>
         </div>
 
-        <div className='form-group'>
-          <label htmlFor='question'>Question:</label>
-          <textarea
-            onChange={onChange}
-            value={question}
-            required
-            name='question'
-            className='form-control'
-            placeholder='Describe your problem in detail.'
-            rows='5'
-          />
+        <div className='row'>
+          <div className='col-md-4'></div>
+          <div className='form-group col-md-4 text-center'>
+            <label htmlFor='repo font-weight-bold'>Github Repository:</label>
+            <input
+              type='text'
+              value={repo}
+              onChange={onChange}
+              name='repo'
+              className='form-control text-center'
+              placeholder='Add a link to a Github Repository (Optional)'
+            />
+          </div>
+          <div className='col-md-4'></div>
         </div>
-        <div className='form-group'>
-          <label htmlFor='repo'>Github Repository:</label>
-          <input
-            type='text'
-            value={repo}
-            onChange={onChange}
-            name='repo'
-            className='form-control'
-            placeholder='Add a link to a Github Repository (Optional)'
-          />
-        </div>
+
         <div
           className='forButton d-flex justify-content-center mt-2'
           style={{ width: "100%" }}
         >
-          <input
-            className='btn btn-mariner text-white'
-            style={{ marginBottom: "60px" }}
-            type='submit'
-          />
+          <input className='btn btn-mariner text-white mb-5' type='submit' />
         </div>
       </form>
     </div>
